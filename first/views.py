513dbb03b48cfdc7de0664a483dbbebc182dae75
enockdeghost <enockdeghost@gmail.com>
2026-04-17 12:30:00 +0300

    take userinput in browers in serach

--- first/views.py
+++ first/views.py
@@ -4,8 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,"first/hello.html")
+
 def enock(request):
     return HttpResponse("enock")
 
 def name(request,names):
-    return HttpResponse(f"hello {names.capitalize()}")
+    return render(request, "first/name.html",{
+        "name":names.upper()
+    })
